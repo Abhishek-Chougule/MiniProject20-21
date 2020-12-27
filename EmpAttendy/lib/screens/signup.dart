73063a96a58e2b0c0bf1909729800c19a50a8f73
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2020-12-27 21:01:05 +0530

    image picker final

--- EmpAttendy/lib/screens/signup.dart
+++ EmpAttendy/lib/screens/signup.dart
@@ -3,40 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:EmpAttendy/screens/CustomInputBox.dart';
 import 'package:image_picker/image_picker.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var scrWidth = MediaQuery.of(context).size.width;
     var scrHeight = MediaQuery.of(context).size.height;
-    File _image;
-
-    _imgFromGallery() async {
-      final File image = await ImagePicker.pickImage(
-          source: ImageSource.gallery, imageQuality: 50) as File;
-      _image = image;
-    }
-
-    void _showPicker(context) {
-      showModalBottomSheet(
-          context: context,
-          builder: (BuildContext bc) {
-            return SafeArea(
-              child: Container(
-                child: new Wrap(
-                  children: <Widget>[
-                    new ListTile(
-                        leading: new Icon(Icons.photo_library),
-                        title: new Text('Photo Library'),
-                        onTap: () {
-                          _imgFromGallery();
-                          Navigator.of(context).pop();
-                        }),
-                  ],
-                ),
-              ),
-            );
-          });
-    }
 
     var safeArea = SafeArea(
       child: Scaffold(
@@ -79,45 +70,20 @@ class SignUpPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 32,
-                      ),
-                      Center(
-                        child: GestureDetector(
-                          onTap: () {
-                            _showPicker(context);
-                          },
-                          child: CircleAvatar(
-                            radius: 55,
-                            backgroundColor: Color(0xffFDCF09),
-                            child: _image != null
-                                ? ClipRRect(
-                                    borderRadius: BorderRadius.circular(50),
-                                    child: Image.file(
-                                      _image,
-                                      width: 100,
-                                      height: 100,
-                                      fit: BoxFit.fitHeight,
-                                    ),
-                                  )
-                                : Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.grey[200],
-                                        borderRadius:
-                                            BorderRadius.circular(50)),
-                                    width: 100,
-                                    height: 100,
-                                    child: Icon(
-                                      Icons.camera_alt,
-                                      color: Colors.grey[800],
-                                    ),
-                                  ),
+                  ClipOval(
+                    child: _image == null
+                        ? Text('No image selected.')
+                        : Image.file(
+                            _image,
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
                           ),
-                        ),
-                      )
-                    ],
+                  ),
+                  FloatingActionButton(
+                    onPressed: getImage,
+                    tooltip: 'Pick Image',
+                    child: Icon(Icons.add_a_photo),
                   ),
                   //
                   SizedBox(

f1a2448e9ac506af875145f7b593f3b79653b12e
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2020-12-29 15:12:21 +0530

    removed contact permission

--- EmpAttendy/lib/screens/main_drawer.dart
+++ EmpAttendy/lib/screens/main_drawer.dart
@@ -6,7 +6,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
     'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
+    //'https://www.googleapis.com/auth/contacts.readonly',
   ],
 );
 

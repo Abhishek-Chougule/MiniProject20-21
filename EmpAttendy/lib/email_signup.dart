6171ebbf27d553205048f0f077cf1f7f695d32f0
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2021-01-03 11:41:11 +0530

    sign up

--- EmpAttendy/lib/email_signup.dart
+++ EmpAttendy/lib/email_signup.dart
@@ -24,159 +24,211 @@ class _EmailSignUpState extends State<EmailSignUp> {
 
   @override
   Widget build(BuildContext context) {
+    var scrWidth = MediaQuery.of(context).size.width;
+    var scrHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(title: Text("Sign Up")),
         body: Form(
             key: _formKey,
             child: SingleChildScrollView(
-                child: Column(children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: "Enter User Name",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter User Name';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Enter Email",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter an Email Address';
-                    } else if (!value.contains('@')) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: mnoController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Enter Mobile Number",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Mobile Number';
-                    } else if (value.length != 10) {
-                      return 'Please enter 10 digit Mobile Number';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: genderController,
-                  decoration: InputDecoration(
-                    labelText: "Enter Gender",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Gender';
-                    } else if (!value.contains('male') &&
-                        !value.contains('Male') &&
-                        !value.contains('MALE') &&
-                        !value.contains('female') &&
-                        !value.contains('Female') &&
-                        !value.contains('FEMALE')) {
-                      return 'Please enter a valid Gender';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: ageController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Enter Age",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Age';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Enter Password",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Password';
-                    } else if (value.length < 6) {
-                      return 'Password must be atleast 6 characters!';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: isLoading
-                    ? CircularProgressIndicator()
-                    : RaisedButton(
-                        color: Colors.lightBlue,
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            setState(() {
-                              isLoading = true;
-                            });
-                            registerToFb();
-                          }
-                        },
-                        child: Text('Submit'),
+                child: Stack(children: [
+              Column(children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 40.0, top: 40),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'Cardo',
+                        fontSize: 35,
+                        color: Color(0xff0C2551),
+                        fontWeight: FontWeight.w900,
                       ),
-              )
+                    ),
+                    //
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, top: 5, bottom: 10),
+                    child: Text(
+                      'For Admin Only',
+                      style: TextStyle(
+                        fontFamily: 'Nunito Sans',
+                        fontSize: 12,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      labelText: "Enter User Name",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter User Name';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Enter Email",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter an Email Address';
+                      } else if (!value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: mnoController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Enter Mobile Number",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter Mobile Number';
+                      } else if (value.length != 10) {
+                        return 'Please enter 10 digit Mobile Number';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: genderController,
+                    decoration: InputDecoration(
+                      labelText: "Enter Gender",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter Gender';
+                      } else if (!value.contains('male') &&
+                          !value.contains('Male') &&
+                          !value.contains('MALE') &&
+                          !value.contains('female') &&
+                          !value.contains('Female') &&
+                          !value.contains('FEMALE')) {
+                        return 'Please enter a valid Gender';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: ageController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Enter Age",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter Age';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Enter Password",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Enter Password';
+                      } else if (value.length < 6) {
+                        return 'Password must be atleast 6 characters!';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: isLoading
+                      ? CircularProgressIndicator()
+                      : RaisedButton(
+                          color: Colors.lightBlue,
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              registerToFb();
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                ),
+              ]),
+              ClipPath(
+                clipper: OuterClippedPart(),
+                child: Container(
+                  color: Colors.blue,
+                  width: scrWidth,
+                  height: scrHeight,
+                ),
+              ),
+              //
+              ClipPath(
+                clipper: InnerClippedPart(),
+                child: Container(
+                  color: Color(0xff0C2551),
+                  width: scrWidth,
+                  height: scrHeight,
+                ),
+              ),
             ]))));
   }
 
@@ -227,5 +279,47 @@ class _EmailSignUpState extends State<EmailSignUp> {
     genderController.dispose();
     passwordController.dispose();
     ageController.dispose();
+  }
+}
+
+class OuterClippedPart extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    //
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height / 4);
+    //
+    path.cubicTo(size.width * 0.55, size.height * 0.16, size.width * 0.85,
+        size.height * 0.05, size.width / 2, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class InnerClippedPart extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    //
+    path.moveTo(size.width * 0.7, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height * 0.1);
+    //
+    path.quadraticBezierTo(
+        size.width * 0.8, size.height * 0.11, size.width * 0.7, 0);
+
+    //
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }

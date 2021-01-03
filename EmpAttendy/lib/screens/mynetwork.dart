48cc6c7f397d8b36c52ff2dc46a4e31d4eea4f39
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2021-01-03 15:47:15 +0530

    network

--- EmpAttendy/lib/screens/mynetwork.dart
+++ EmpAttendy/lib/screens/mynetwork.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -27,11 +28,50 @@ class MyNetwork extends StatefulWidget {
 class _MyNetworkState extends State<MyNetwork> {
   String _macAddress = "Unknown";
   String _imeiNumber = "Unknown";
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = new Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  String wifiname;
+  String status = 'No Connected !';
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      setState(() => _connectionStatus = result.toString());
+    });
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  Future<Null> initConnectivity() async {
+    String connectionStatus;
+
+    try {
+      connectionStatus = (await _connectivity.checkConnectivity()).toString();
+    } on PlatformException catch (e) {
+      print(e.toString());
+      connectionStatus = 'Failed to get connectivity.';
+    }
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _connectionStatus = connectionStatus;
+      wifiname = 'Connection Status: $_connectionStatus';
+      if (wifiname == 'Connection Status: ConnectivityResult.wifi') {
+        status = 'Connected';
+      }
+    });
   }
 
   Future<void> initPlatformState() async {
@@ -63,9 +103,13 @@ class _MyNetworkState extends State<MyNetwork> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("MAC Address :  " + _macAddress),
-              Text(''),
-              Text("IMEI Number :  " + _imeiNumber),
+              Text((() {
+                if (status == 'Connected') {
+                  return _macAddress + _imeiNumber;
+                } else {
+                  return status;
+                }
+              })()),
             ],
           ),
         ));

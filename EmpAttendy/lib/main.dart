45997704ec0bbcee87ef349c59adf07e021242d0
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2021-01-04 18:55:39 +0530

    ss changes

--- EmpAttendy/lib/main.dart
+++ EmpAttendy/lib/main.dart
@@ -31,12 +31,12 @@ class IntroScreen extends StatelessWidget {
     User result = FirebaseAuth.instance.currentUser;
     return new SplashScreen(
         navigateAfterSeconds: result != null ? Home(uid: result.uid) : SignUp(),
-        seconds: 5,
+        seconds: 4,
         title: new Text(
           'Welcome To EmpAttendy',
           style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
-        image: Image.asset('assets/images/dart.png', fit: BoxFit.scaleDown),
+        image: Image.asset('assets/sslogo.png', fit: BoxFit.scaleDown),
         backgroundColor: Colors.white,
         styleTextUnderTheLoader: new TextStyle(),
         photoSize: 100.0,

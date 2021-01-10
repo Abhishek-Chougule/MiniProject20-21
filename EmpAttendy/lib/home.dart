2136c8b0b2249dceb8ef95194f08f276c63a4116
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2021-01-10 23:29:01 +0530

    fix

--- EmpAttendy/lib/home.dart
+++ EmpAttendy/lib/home.dart
@@ -6,7 +6,6 @@ import 'package:EmpAttendy/screens/wificonnectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:EmpAttendy/screens/FingerPrintLock.dart';
 
 class Home extends StatefulWidget {
   Home({this.uid});

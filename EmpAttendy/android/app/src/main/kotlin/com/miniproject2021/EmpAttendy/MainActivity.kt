536fa2cc38b7660362da8fc8553ab4a740f6bc6e
An0nym0us-MrAbhi <chouguleabhis@gmail.com>
2021-01-10 11:40:40 +0530

    fix

--- EmpAttendy/android/app/src/main/kotlin/com/miniproject2021/EmpAttendy/MainActivity.kt
+++ EmpAttendy/android/app/src/main/kotlin/com/miniproject2021/EmpAttendy/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.miniproject2021.EmpAttendance
-package com.miniproject2021.EmpAttendance
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterFragmentActivity
